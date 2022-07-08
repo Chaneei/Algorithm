1ee956d422bf382e7f9b7899142a6e754efce48f
Seung Chan Lee <96166013+Chaneei@users.noreply.github.com>
2022-07-08 21:02:21 +0900

    Add files via upload

---
+++ Algo/41_Unique_in_Order.js
@@ -0,0 +1,12 @@
+function uniqueInOrder(it) {
+  let result = [];
+  let last;
+
+  for (let i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push((last = it[i]));
+    }
+  }
+
+  return result;
+}
