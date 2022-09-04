de12da4b382307032934e517390d59eeaa80596c
Seung Chan Lee <96166013+Chaneei@users.noreply.github.com>
2022-09-04 16:55:05 +0900

    Anagram_Detection.js

---
+++ Algo/Anagram_Detection.js
@@ -0,0 +1,6 @@
+
+var isAnagram = function (test, original) {
+  let a = test.toLowerCase().split("").sort().join("");
+  let b = original.toLowerCase().split("").sort().join("");
+  return a == b;
+};
